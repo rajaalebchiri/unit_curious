015fb25127d97c3a1f42635baa81b69070407f40
rajaalebchiri <rajaa.lebchiri@gmail.com>
2024-03-04 21:23:17 +0100

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="unit_curious",
-    version="0.0.2",
+    version="0.0.3",
     description="Explore historical, quirky, and unconventional units of measurement with this easy-to-use Python library.",
     long_description=long_description,
     long_description_content_type="text/markdown",
