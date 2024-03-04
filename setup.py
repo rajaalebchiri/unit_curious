b10a2734134a430ea4d2c4a5216e7d0b640d478e
rajaalebchiri <rajaa.lebchiri@gmail.com>
2024-03-04 20:50:12 +0100

    fixing long description +  fixing workflows

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="unit_curious",
     version="0.0.2",
+    description="Explore historical, quirky, and unconventional units of measurement with this easy-to-use Python library.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=["contrib", "docs", "tests*"]),
 )
